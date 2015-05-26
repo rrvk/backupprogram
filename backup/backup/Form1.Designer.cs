88bc264f94a1cd5f66152a03ac6ade28e1ebfb9b
rrvk <rvankammen@live.nl>
2015-05-26 10:05:02 +0200

    Begin

---
+++ backup/backup/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace backup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbackup = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.asdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstbackup
+            // 
+            this.lstbackup.FormattingEnabled = true;
+            this.lstbackup.Location = new System.Drawing.Point(12, 26);
+            this.lstbackup.Name = "lstbackup";
+            this.lstbackup.Size = new System.Drawing.Size(157, 173);
+            this.lstbackup.TabIndex = 0;
+            this.lstbackup.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lstbackup_KeyDown);
+            this.lstbackup.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lstbackup_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Wat te backupen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Backup locatie";
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoEllipsis = true;
+            this.lblLocation.Location = new System.Drawing.Point(175, 26);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(154, 15);
+            this.lblLocation.TabIndex = 3;
+            this.lblLocation.Text = "C:\\backup test";
+            this.lblLocation.Click += new System.EventHandler(this.lblLocation_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(178, 174);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(84, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start backup";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // asdToolStripMenuItem
+            // 
+            this.asdToolStripMenuItem.Name = "asdToolStripMenuItem";
+            this.asdToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoEllipsis = true;
+            this.lblProgress.Location = new System.Drawing.Point(175, 156);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(154, 15);
+            this.lblProgress.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 209);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblLocation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstbackup);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbackup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ToolStripMenuItem asdToolStripMenuItem;
+        private System.Windows.Forms.Label lblProgress;
+    }
+}
+
---
+++ backup/backup/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace backup
+{
+    public partial class Form1 : Form
+    {
+        private String savePath;
+        private readonly String addString = "Add new entry";
+        private ContextMenuStrip listboxContextMenu;
+        private String lastFolder;
+        delegate void SetTextCallback(string text);
+        public Form1()
+        {
+            InitializeComponent();
+            // make the right click on the lstbox
+            listboxContextMenu = new ContextMenuStrip();
+            listboxContextMenu.Opening += new CancelEventHandler(listboxContextMenu_Opening);
+            listboxContextMenu.ItemClicked += listboxContextMenu_ItemClicked;
+            lstbackup.ContextMenuStrip = listboxContextMenu;
+            savePath = lblLocation.Text.ToString();
+        }
+
+        private void setProgressLabel(String tekst)
+        {
+            if (this.lblProgress.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(setProgressLabel);
+                this.Invoke(d, new object[] { tekst });
+            }
+            else
+            {
+                lblProgress.Text = tekst;
+            }
+        }
+
+        private void listboxContextMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem.ToString() == addString)
+            {
+                FolderBrowserDialog folder = new FolderBrowserDialog();
+                if (folder.ShowDialog() == DialogResult.OK)
+                {
+                    lstbackup.Items.Add(folder.SelectedPath);
+                }
+            }
+            else if (e.ClickedItem.ToString().Contains("Remove"))
+            {
+                lstbackup.Items.RemoveAt(lstbackup.SelectedIndex);
+            }
+        }
+
+        private void lblLocation_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                savePath = folder.SelectedPath;
+                lblLocation.Text = savePath;
+            }
+        }
+
+        private void lstbackup_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                lstbackup.SelectedIndex = lstbackup.IndexFromPoint(e.Location);
+                listboxContextMenu.Show();  
+            }
+        }
+
+        private void listboxContextMenu_Opening(object sender, CancelEventArgs e)
+        {
+            //clear the menu and add custom items
+            listboxContextMenu.Items.Clear();
+            listboxContextMenu.Items.Add(addString);
+            if (lstbackup.SelectedIndex != -1)
+            {
+                listboxContextMenu.Items.Add(string.Format("Remove - {0}", lstbackup.SelectedItem.ToString()));
+            }
+        }
+
+        private void lstbackup_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (lstbackup.SelectedIndex != -1)
+                {
+                    lstbackup.Items.RemoveAt(lstbackup.SelectedIndex);
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Thread workerThread = new Thread(checkBackupSettings);
+            workerThread.Start();
+        }
+
+        private void checkBackupSettings()
+        {
+            if (lstbackup.Items.Count == 0)
+            {
+                MessageBox.Show("Geen backup locaties");
+                return;
+            }
+            if (Directory.Exists(savePath))
+            {
+                foreach (String path in lstbackup.Items)
+                {
+                    if (Directory.Exists(path))
+                    {
+                        // TODO DIT NOG CROSS THREAD
+                        //btnStart.Enabled = false;
+                        backUp(path);
+                    }
+                    else
+                    {
+                        var ant = MessageBox.Show(path + " does not exist do you want to continue?", "Continue?", MessageBoxButtons.YesNo);
+                        if (ant == DialogResult.No) { /*// TODO DIT NOG CROSS THREADbtnStart.Enabled = true; */return; }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("The save directory does not exist");
+            }
+            // TODO DIT NOG CROSS THREAD
+            //btnStart.Enabled = true;
+        }
+
+        private void backUp(String path)
+        {
+            //String[] filePath = Directory.GetFiles(path,"*.*",SearchOption.AllDirectories);
+            // first get the root folder
+            string dirName = new DirectoryInfo(path).Name;
+            // check if root directory exist in backup location
+            checkFolderAndCreate(Path.Combine(savePath, dirName));
+            // then every folder within
+            // loop through all the maps
+            lastFolder = path;
+            checkDirectorys(new DirectoryInfo(path).GetDirectories());
+            /*foreach(String file in filePath){
+                dirName = new DirectoryInfo(file).Name;
+                setProgressLabel(file);
+                Console.WriteLine(file);
+            }*/
+        }
+        
+
+        private void checkDirectorys(DirectoryInfo[] folders)
+        {
+            foreach (DirectoryInfo dir in folders)
+            {
+                DirectoryInfo[] supFolders = new DirectoryInfo(dir.FullName).GetDirectories();
+                if (supFolders.Length > 0)
+                {
+                    checkDirectorys(supFolders);
+                }
+                Console.WriteLine(dir.Name);
+                //DirectoryInfo[] supFolders = new DirectoryInfo(dir.Name);
+            }
+        }
+
+        private void checkFolderAndCreate(String path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
